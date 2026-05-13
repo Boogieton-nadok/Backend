cd1d82a6aa05403ed6dfed22f3bb7b749ab37f5b
jmyoo0512 <jm030512@gmail.com>
2026-05-14 00:28:57 +0900

    feat: emotion repository 작성

---
+++ src/main/java/com/boogieton/nadok/domain/emotion/repository/CharacterRepository.java
@@ -0,0 +1,6 @@
+package com.boogieton.nadok.domain.emotion.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+}
---
+++ src/main/java/com/boogieton/nadok/domain/emotion/repository/EmotionInputRepository.java
@@ -0,0 +1,7 @@
+package com.boogieton.nadok.domain.emotion.repository;
+
+import com.boogieton.nadok.domain.emotion.entity.EmotionInput;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmotionInputRepository extends JpaRepository<EmotionInput, Long> {
+}
