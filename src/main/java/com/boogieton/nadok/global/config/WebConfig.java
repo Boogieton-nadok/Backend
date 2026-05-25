10015c838809ce004a582a842b042dfb81a8e0af
tack <shy66681@naver.com>
2026-05-25 15:42:04 +0900

    fix: 업로드 경로 수정

--- src/main/java/com/boogieton/nadok/global/config/WebConfig.java
+++ src/main/java/com/boogieton/nadok/global/config/WebConfig.java
@@ -7,20 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    //CORS
+
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowCredentials(true)
                 .allowedOrigins("http://localhost:5173", "https://ideatone-fe.vercel.app/")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowCredentials(true);
-
     }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/home/ubuntu/nadok/uploads/");
+        // 💡 /uploads/profiles/ 주소로 호출했을 때 실제 서버의 내부 폴더를 정확하게 바라보도록 슬래시(/) 끝처리를 보완합니다.
+        registry.addResourceHandler("/uploads/profiles/**")
+                .addResourceLocations("file:/home/ubuntu/nadok/uploads/profiles/");
     }
 }
