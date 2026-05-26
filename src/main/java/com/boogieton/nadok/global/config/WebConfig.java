784f828eb8fbfb71140d8a80dcf5fc5822b0df9d
tack <shy66681@naver.com>
2026-05-26 16:43:23 +0900

    CORS 추가

--- src/main/java/com/boogieton/nadok/global/config/WebConfig.java
+++ src/main/java/com/boogieton/nadok/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://ideatone-fe.vercel.app/")
+                .allowedOrigins("http://localhost:5173", "https://ideatone-fe.vercel.app/", "https://bugitone-3rby.vercel.app/")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowCredentials(true);
